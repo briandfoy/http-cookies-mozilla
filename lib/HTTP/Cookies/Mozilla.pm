8cbe846efea225f26582dbcb33bc488b232d1987
brian d foy <brian.d.foy@gmail.com>
2016-01-27 14:01:00 -0500

    bump version

--- lib/HTTP/Cookies/Mozilla.pm
+++ lib/HTTP/Cookies/Mozilla.pm
@@ -83,7 +83,7 @@ use Carp qw(carp);
 use constant TRUE  => 'TRUE';
 use constant FALSE => 'FALSE';
 
-$VERSION = '2.01';
+$VERSION = '2.011';
 $SQLITE = 'sqlite3';
 
 my $EPOCH_OFFSET = $^O eq "MacOS" ? 21600 : 0;  # difference from Unix epoch
